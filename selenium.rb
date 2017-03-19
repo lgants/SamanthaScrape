55edc6aff81678a6148aefc7d458fc95d836503f
tthibodeaux <tthibodeaux@NRLIDGMBTTHIBO.local>
2017-03-19 13:29:42 -0500

    Watir version getting search page

--- selenium.rb
+++ selenium.rb
@@ -8,10 +8,7 @@ require 'phantomjs/poltergeist'
 include Capybara::DSL
 Capybara.default_driver = :poltergeist
 
-visit "https://cotthosting.com/laplaquemines/LandRecords/protected/SrchQuickName.aspx"
+visit "https://cotthosting.com/laplaquemines/User/Login.aspx?ReturnUrl=%2flaplaquemines%2fLandRecords%2fprotected%2fSrchQuickName.aspx"
+click_button('ctl00_cphMain_blkLogin_btnLogin')
 
-
-all('#ctl00_cphMain_blkLogin_btnGuestLogin').each do |field|
-  puts field
-
-end
+print page.html
